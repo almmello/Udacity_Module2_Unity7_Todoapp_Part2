4938c9e10840cfb7e5803f2ad4279c15330b80f4
Alexandre <almmello@gmail.com>
2022-06-09 19:08:39 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://almmello@localhost:5432/todos'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    __tablename__ = 'todos'
+    id = db.Column(db.Integer, primary_key=True)
+    description = db.Column(db.String(), nullable=False)
+
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+
+db.create_all()
+
+@app.route('/')
+def index():
+  return render_template('index.html', data=Todo.query.all()
+  )
+
+
+#always include this at the bottom of your code (port 3000 is only necessary in workspaces)
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", port=3000)
